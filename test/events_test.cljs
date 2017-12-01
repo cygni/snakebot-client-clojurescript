a844b4f00ce5584665cd56ce17910a0b6ce2103a
Axel <axelskyttner@gmail.com>
2017-12-01 09:12:29 +0100

    changed name

--- test/events_test.cljs
+++ test/events_test.cljs
@@ -2,6 +2,7 @@
   (:require [cljs.test :refer-macros [async deftest is testing]]
             [cljs-snake-bot.settings :as s]
             [cljs-snake-bot.events :as e]))
+;mock of message
 (def incoming-message {
                        :name "axel"
                        :player-color "green"
@@ -9,11 +10,11 @@
                        })
 
 
-(deftest setting-state
+(deftest state-set-many-test 
     ( s/state-set-many incoming-message)
     (is (= "green"  (s/state-get :player-color))))
 
-(deftest on-player-registered-check
+(deftest on-player-registered-check-test
     ( e/on-player-registered incoming-message)
     (is (= "123"  (s/state-get :player-id))))
 
